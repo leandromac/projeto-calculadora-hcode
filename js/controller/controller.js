9d643bba95417085ef338f52402465c230229594
leandromac <contato@leandromacedo.com>
2020-08-18 18:37:48 -0300

    Incluir som nos botões

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,6 +1,8 @@
 class CalcController {
 
   constructor() {
+    this._audio = new Audio('click.mp3');
+    this._audioOnOff = false;
     this._lastOperator = '';
     this._lastNumber = '';
     this._operation = [];
@@ -37,11 +39,26 @@ class CalcController {
     }, 1000);
     this.setLastNumberToDisplay();
     this.pasteFromClipboard();
+    document.querySelectorAll('.btn-ac').forEach(btn => {
+      btn.addEventListener('dblclick', e => {
+        this.toggleAudio();
+      });
+    });
   } 
+
+  toggleAudio() {
+    this._audioOnOff = !this._audioOnOff;
+  }
+  playAudio() {
+    if(this._audioOnOff) {
+      this._audio.currentTime = 0;
+      this._audio.play();
+    }
+  }
 
   initKeyboard() {
     document.addEventListener('keyup', e => {
-      console.log(e.key);
+      this.playAudio();
       switch(e.key) {
         case 'Escape':
           this.clearAll();
@@ -206,6 +223,7 @@ class CalcController {
   }
 
   execBtn(value) {
+    this.playAudio();
     switch (value) {
       case 'ac':
         this.clearAll();
